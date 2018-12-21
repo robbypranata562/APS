b06a5261c3681df776615c56ffa87fe59b638dcc
egi apriandi <egiapriandi361@gmail.com>
2018-12-21 16:59:40 +0700

    product js

--- product.php
+++ product.php
@@ -14,47 +14,46 @@ include(__DIR__."/main_header.php");
 <script src="product/js/underscores.min.js"></script>
 <script src="product/js/uikit.js"></script>
 <script src="product/js/product.js"></script>
+<script src="global_assets/js/plugins/forms/styling/switchery.min.js"></script>
 
 		<!-- /Custom Javascript -->
 		<?php include(__DIR__."/navbar_sidebar.php"); ?>
 			<!-- Main content -->
-			<div class="content-wrapper">
-
-				<!-- Page header -->
-				<div class="page-header">
-					<div class="page-header-content header-elements-md-inline">
-						<div class="page-title d-flex">
-							<p>Product</p>
+			<div class="content-wrapper" data-uk-productpage>
+				<!-- Page Container Selector-->
+				<div class="aps-container">
+					<!-- Page header -->
+					<div class="page-header">
+						<div class="page-header-content header-elements-md-inline">
+							<div class="page-title d-flex">
+								<p>Product</p>
+							</div>
+							<div class="btn-toolbar justify-content-center">
+								<button type="button" class="btn btn-primary btn-md mr-2 aps-add-product"><i class="icon-plus22 mr-2"></i> Produk</button>
+								<button type="button" class="btn btn-primary btn-md aps-export-product">Ekspor</button>
+							</div>
 						</div>
 					</div>
-				</div>
-				<!-- /page header -->
-				
-				<!-- Content area -->
-				<div class="content pt-0">
+					<!-- /page header -->
+					
+					<!-- Content area -->
+					<div class="content pt-0">
 
-					<!-- Dashboard content -->
-					<div class="row" data-uk-aps>
-						<div class="coba col-xl-8">
-							Pilih outlet : 
-							<select id="pilihanoutlet">
-							</select>
-							<br/>
-							<br/>
-							<p>pilihan outlet otomatis tersimpan dan menunjukan outlet yang terakhir di pilih <== ini digunakan pada setiap halaman yang wajib ada pilihan outlet nya</p>
-							<p><strong>Boleh coba di refresh</strong></p>
+						<!-- Dashboard content -->
+						<div class="row">
+							<div class="col-xl-8"></div>
 						</div>
+						<!-- /dashboard content -->
+
 					</div>
-					<!-- /dashboard content -->
-
+					<!-- /content area -->
 				</div>
-				<!-- /content area -->
-
+				<!-- /Page Container Selector-->
 			</div>
 			<!-- /main content -->
 
 		</div>
 	<!-- /page content -->
-	<?php include(__DIR__."/product/views/template.html");?>
+	<?php include(__DIR__."/product/views/add-product.html");?>
 	</body>
 </html>
--- product/js/product.js
+++ product/js/product.js
@@ -1,12 +1,41 @@
 (function (apsCore, util, $) {
-	var $u = util.$,
-        attr = util.attr,
-        css = util.css,
-        addClass = util.addClass;
+	var $u 			= util.$,
+        attr 		= util.attr,
+        css 		= util.css,
+        addClass 	= util.addClass;
 		
-	window.ap = window.ap || {};
+	window.aps = window.aps || {};
+	
+	aps.apiURL = 'https://development.autopilotstore.co.id/api_all.php';
+	
+	aps.noop = function( message ){
+		alert( message );
+	};
+	
+	aps.req = function( params ){
+		var dataString = JSON.stringify( params );
+		return new util.Promise( function(resolve, reject){
+			$.ajax({
+				type: "POST",
+				url: aps.apiURL,
+				data: dataString,
+				success: function( data ){
+					if( data.errcode == "OK" ){
+						resolve( data );
+					} else {
+						reject( data.message );
+					}
+					
+				},
+				error:function(){
+					reject( 'error xhr' );
+				},
+				dataType: 'json',
+			});
+		});
+	};
 
-	apsCore.component( 'aps', {
+	apsCore.component( 'productpage', {
 		connected:function(){
 			this.init();
 		},
@@ -19,20 +48,59 @@
 		events:[
 			{
 				name:'click',
-				delegate:'.coba',
+				delegate:'.aps-add-product',
 				handler:function(e){
 					e.preventDefault();
-					alert()
+					this.addProduct();
 				}
 			}
 		],
 		methods:{
+			init:function(){},
+			addProduct:function(){
+				this.render( $( '#tmpl-add-product' ), {} );
+			},
+			render:function( $template, data ){
+				var tmp = _.template( $template.html() );
+				$( this.$el, $( '.aps-container' ) ).html( tmp( data ) );
+			}
+		}
+	});
+	
+	apsCore.component( 'switchery', {
+		connected:function(){
+			this.init();
+		},
+		methods:{
 			init:function(){
-				var tmp = _.template( $( 'script#tmpl-contoh' ).html() );
-				
-				$( this.$el ).append(tmp({name:'hahaha'}));
+				new Switchery(this.$el);
 			},
 		}
 	});
+	
+	apsCore.component( 'selectproductcategory', {
+		connected:function(){
+			this.init();
+		},
+		methods:{
+			init:function(){
+				aps.req( {tipe:'GET_KATEGORIPRODUK'} )
+					.then(
+						function(data){
+							console.log(data)
+						},
+						aps.noop
+					);
+			},
+		}
+	});
+	
+	aps.req( {tipe:'GET_OUTLET', idoutlet:''} )
+		.then(
+			function(data){
+				console.log(data)
+			},
+			aps.noop
+		);
 	
 })(UIkit, UIkit.util, jQuery)
