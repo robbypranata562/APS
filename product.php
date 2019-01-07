7e2e24b8f8df2e85fb66c966a7f266239dc9d53c
egi apriandi <egiapriandi361@gmail.com>
2019-01-07 08:50:56 +0700

    szxdcf

--- product.php
+++ product.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 include(__DIR__."/Functions.php");
-include(__DIR__."/cekUser.php");
-include(__DIR__."/DBConnectPDOH.php");
+//include(__DIR__."/cekUser.php");
+//include(__DIR__."/DBConnectPDOH.php");
 include(__DIR__."/product/views/table-product.html");
 $page_title = 'Product Page';
 include(__DIR__."/main_header.php");
